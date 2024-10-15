9e024f2db5c4102526e75ae9016e4f59f9158e26
Gre-mie <meow31-cat@outlook.com>
2024-10-15 16:33:02 +0100

    Update: funciton to count how many times I use an emoji in the top 10

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import re
 
-# List of top 10 emojis of 2023 from: https://www.meltwater.com/en/blog/top-emojis-2023
-top_ten_emojis = ['😊', '✨', '🥰', '😍', '🙏', '🔥', '❤️', '🤣', '😭', '😂']
+file_paths = {
+            "boot.dev": ["messages/boot.dev/october.txt", "messages/boot.dev/september.txt"],
+            "donthedeveloper": ["messages/donthedeveloper/october.txt", "messages/donthedeveloper/september.txt"]
+            }
 
 def open_file(file_path):
     try:
@@ -10,22 +12,40 @@ def open_file(file_path):
     except FileNotFoundError:
         print(f"\n\033[38;5;{172}m{file_path} \033[31mFile Not Found\n\033[39m...")
     
+def count_which_popular_emojis_I_use(file_paths):
+    # List of top 10 emojis of 2023 from: https://www.meltwater.com/en/blog/top-emojis-2023
+    top_ten_emojis = {'😊': 0, '✨': 0, '🥰': 0, '😍': 0, '🙏': 0, '🔥': 0, '❤️': 0, '🤣': 0, '😭': 0, '😂': 0}
+
+    # iterates through all the files in file_paths
+    for discord in file_paths:
+        for path in file_paths[discord]:
+            current_file = open_file(path)
+
+            # increments an emoji counter in the top_ten_emojis, dict each time it finds a matching emoji in the files
+            for char in current_file:
+                if char in top_ten_emojis:
+                    top_ten_emojis[char] += 1
+
+    return top_ten_emojis
+
+
 
 
 
 
 def main():
-    file_paths = {
-            "boot.dev": ["messages/boot.dev/october.txt", "messages/boot.dev/september.txt"],
-            "donthedeveloper": ["messages/donthedeveloper/october.txt", "messages/donthedeveloper/september.txt"]
-            }
+    #boot.dev_emoji_count = 
+    
+    popular_emojis = count_which_popular_emojis_I_use(file_paths)
+    print(popular_emojis) # test
 
-    for discord in file_paths:
-        for path in file_paths[discord]:
-            current_file = open_file(path)
-            #print(current_file)
+
+
+
         
 
+
+    
         
         
 
