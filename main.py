c481dff6a18b534d9689c5e0f8f01f9fae7600a7
Gre-mie <meow31-cat@outlook.com>
2024-10-15 16:11:57 +0100

    Update: add fuction that opens files

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+import re
+
+# List of top 10 emojis of 2023 from: https://www.meltwater.com/en/blog/top-emojis-2023
+top_ten_emojis = ['😊', '✨', '🥰', '😍', '🙏', '🔥', '❤️', '🤣', '😭', '😂']
+
+def open_file(file_path):
+    try:
+        with open(file_path) as text_string:
+            return text_string.read()
+    except FileNotFoundError:
+        print(f"\n\033[38;5;{172}m{file_path} \033[31mFile Not Found\n\033[39m...")
+    
 
 
 
 
 def main():
-    print("working")
+    file_paths = {
+            "boot.dev": ["messages/boot.dev/october.txt", "messages/boot.dev/september.txt"],
+            "donthedeveloper": ["messages/donthedeveloper/october.txt", "messages/donthedeveloper/september.txt"]
+            }
+
+    for discord in file_paths:
+        for path in file_paths[discord]:
+            current_file = open_file(path)
+            #print(current_file)
+        
+
+        
+        
+
 
 
 
